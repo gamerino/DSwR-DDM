1c20d3b40d164829cd3beb28fb03e418ae274dc5
Gabriela Merino <30473582+gamerino@users.noreply.github.com>
2019-08-04 23:48:11 -0300

    Update scriptLCdP_DDM.R

--- scriptLCdP_DDM.R
+++ scriptLCdP_DDM.R
@@ -1,4 +1,5 @@
-setwd("/Users/gabycleta/Google Drive File Stream/Mi unidad/DatosDeMiercoles/")
+
+setwd("/myDirectory/DatosDeMiercoles/")
 # install_packages("readr")
 library(readr)
 library(dplyr)
@@ -8,28 +9,33 @@ library(ggpubr)
 library(cowplot)
 
 la_casa_de_papel <- readr::read_csv("https://raw.githubusercontent.com/cienciadedatos/datos-de-miercoles/master/datos/2019/2019-07-31/la_casa_de_papel.csv")
-# remove words such a “a”, “an” or “the”
+# obtener palabras por separado
 la_casa_de_papel_subs <- la_casa_de_papel %>%
     unnest_tokens(word,texto) %>%
     anti_join(stop_words)
-
+# definir los personajes, teniendo en cuenta los "búscados" luego de T2 (https://github.com/gamerino/DSwR-DDM/blob/master/LCDPPersonajes.png)
 personajes<-c("profesor", "tokio", "rio", "denver", "helsinki", "nairobi", 
               "lisboa", "lisboa", "lisboa", "estocolmo", "estocolmo")
+# teniendo en cuenta que raquel=inspectora=lisboa y que mónica=estocolmo
 names(personajes)<-c("profesor", "tokio", "río", "denver", "helsinki", "nairobi", 
               "inspectora", "raquel", "lisboa", "mónica", "estocolmo")
 
-
+#agrupamos los personajes según el número de veces que se los nombra por episodio por temporada
 la_casa_de_papel_pers=la_casa_de_papel_subs %>% filter(word %in%names(personajes)) %>% group_by(word, episodio,temporada) %>% tally()
-la_casa_de_papel_pers=mutate(la_casa_de_papel_pers, wordSumm = personajes[word])
 
+#agrupamos los personajes según su denominación como capital de país ( a parte del profesor :P) 
+la_casa_de_papel_pers=mutate(la_casa_de_papel_pers, wordSumm = personajes[word])
+# determinamos los porcentajes
 la_casa_de_papel_pers_perc=la_casa_de_papel_pers %>%
     group_by(wordSumm,temporada) %>%                     
     dplyr::mutate(sumper=n/sum(n))%>%group_by(wordSumm,temporada,episodio)%>%
     dplyr::summarise(Ret=sum(sumper)*100)
-
+# Gráficos!
 graficos=list()
 for( p in unique(personajes)){
+    # cargamos la imágen del personaje en cuestión
     pen <- png::readPNG(paste(p, ".png", sep=""))
+    # graficamos los porcentajes por temporada y por episodio
     graficos[[p]]=ggplot(la_casa_de_papel_pers_perc %>% filter(wordSumm %in% p), 
                          aes(x=temporada, y=Ret, fill=factor(episodio)))+background_image(as.raster(pen))+geom_bar(color="black",
                              stat = "identity", alpha=0.7)+theme(legend.position = "none")+guides(fill =guide_legend(nrow=1))+labs(
